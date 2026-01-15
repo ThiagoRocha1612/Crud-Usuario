040eeabbf8081767cb9a28823181b9f89778ab48
Thiago Rocha <Thiagomarlon1612@gmail.com>
2026-01-15 17:08:37 -0300

    feat: implementa UsuarioRepository com busca e deleção por email
    - Adicionado método findByEmail e deleteByEmail com suporte a @Transactional.

---
+++ crud-usurario/src/main/java/com/javanaut/crud_usurario/infrastructure/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package com.javanaut.crud_usurario.infrastructure.repository;
+
+import com.javanaut.crud_usurario.infrastructure.entitys.Usuario;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByEmail(String email);
+
+    @Transactional
+    void deleteByEmail(String email);
+}
